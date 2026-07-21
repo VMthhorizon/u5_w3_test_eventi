1d0395607123333970f75c135970ac856b99c22e
Vincenzo Mola <vincenzo.mola@hotmail.com>
2026-07-21 18:52:46 +0200

    Initial commit

---
+++ src/main/java/vincenzomola/u5_w3_test_eventi/U5W3TestEventiApplication.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5_w3_test_eventi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3TestEventiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5W3TestEventiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vincenzomola/u5_w3_test_eventi/U5W3TestEventiApplicationTests.java
@@ -0,0 +1,13 @@
+package vincenzomola.u5_w3_test_eventi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3TestEventiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
